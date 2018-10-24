dafe42dc594ebfdbecc5bc53d032c188a6b5d8ef
narisuke0403 <sungwoop0403@yahoo.co.jp>
2018-10-24 20:30:32 +0900

    1

--- train.py
+++ train.py
@@ -20,13 +20,15 @@ with open("data/teacher.csv", "r") as f:
         teacher_data = list(map(int,row))
 y = np.array(teacher_data)
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
 
 mod = xgboost.XGBRegressor()
-mod.fit(X,y)
-y_train_pred = mod.predict(X)
+mod.fit(X_train,y_train)
+y_train_pred = mod.predict(X_test)
 a = 0
-for i in range(len(y)):
-    a += (math.log(y[i]+1)-math.log(y_train_pred[i]+1))**2
+for i in range(len(y_test)):
+    a += (math.log(y_test[i]+1)-math.log(y_train_pred[i]+1))**2
 score = math.sqrt(a)/len(y)
 from sklearn.metrics import r2_score
 print('xgboost  score_test: {}'.format(score))
