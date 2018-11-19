267434a7d8e6927ed3c5cbe7c0a59d9b2595f3ee
sungwoo <sungwoop0403@yahoo.co.jp>
2018-11-20 00:26:41 +0900

    a

--- preprocess.py
+++ preprocess.py
@@ -1,227 +1,90 @@
-import csv
-import glob
-import os
-import numpy as np
-import math
-import unicodedata
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+data1 = pd.read_csv("data/train.csv")
+teacher = data1["attendance"]
+
+# 無観客試合を除去
+data1 = data1.drop(2479)
+teacher = teacher.drop(2479)
+
+data1 = data1.drop("attendance",axis=1)
+data2 = pd.read_csv("data/test.csv")
 
 
-# ファイル名の更生
-path = "./data/*"
-path_list = glob.glob(path)
-for file in path_list:
-    file_rename = file.replace("'", "").replace("'", "")
-    os.rename(file,file_rename)
+def get_holiday():
+    holiday_list = pd.read_csv("data/holidays_in_japan.csv")
+    return dict(zip(holiday_list["holiday_date"], holiday_list["description"]))
 
-# 訓練データの格納
-train_data = []
-with open("data/train.csv", "r", encoding="utf-8_sig") as f:
-    reader = csv.reader(f)
-    header = next(reader)
+def get_stadium_cap():
+    stadium_cap = pd.read_csv("data/stadium_capacity_mapping.csv")
+    return dict(zip(stadium_cap["stadium"], stadium_cap["capacity"]))
 
-    for row in reader:
-        train_data.append(row)
+def get_data():
+    data = pd.concat([data1, data2])
 
-# テストデータの格納
-test_data = []
-with open("data/test.csv", "r", encoding="utf-8_sig") as f:
-    reader = csv.reader(f)
-    header = next(reader)
+    # スタジアムの収容数と日付をカテゴリ変数化
+    data["stadium_cap"] = data["venue"]
+    data["stadium_cap"] = data["stadium_cap"].replace(get_stadium_cap())
+    data = data.replace(get_holiday())
+    data = data.replace("(.*)-(.*)-(.*)", "normal day", regex=True)
 
-    for row in reader:
-        test_data.append(row)
+    # 時間をリスト化
+    kick_off_time_list = pd.unique(data["kick_off_time"]).tolist()
+    kick_off_time_list.sort()
+    result = [kick_off_time_list[0:28],kick_off_time_list[28:76],kick_off_time_list[76:]]
+    data.loc[data["kick_off_time"].isin(result[0]), "kick_off_time"] = "noon"
+    data.loc[data["kick_off_time"].isin(result[1]), "kick_off_time"] = "evening"
+    data.loc[data["kick_off_time"].isin(result[2]), "kick_off_time"] = "night"
 
-# 休日データの格納
-holiday = []
-with open("data/holidays_in_japan.csv", "r", encoding="utf-8_sig") as f:
-    reader = csv.reader(f)
-    header = next(reader)
+    # セクションをリスト化
+    section = pd.unique(data["section"]).tolist()
+    result = [section[:2], section[2:-2], section[-2:]]
+    data.loc[data["section"].isin(result[0]), "section"] = "start"
+    data.loc[data["section"].isin(result[1]), "section"] = "playing"
+    data.loc[data["section"].isin(result[2]), "section"] = "end"
 
-    for row in reader:
-        holiday.append(row[0])
+    # 放送局カラムを削除
+    data = data.drop("broadcasters", axis=1)
+    
+    # sectionとroundを削除
+    data = data.drop("round", axis=1)
 
-# スタジアム容量の格納
-stadium = {}
-with open("data/stadium_capacity_mapping.csv", "r", encoding="utf_8_sig") as f:
-    reader = csv.reader(f)
-    header = next(reader)
+    # 天気を雨かそれ以外で分割
+    data["weather"] = data["weather"].replace("^(?!.*雨).*$", "not rain", regex=True)
+    data = data.replace("(.*)雨(.*)", "rain", regex=True)
 
-    for row in reader:
-        stadium[row[0]] = int(row[1])
+    # チーム名を正規化
+    data["home_team"] = data["home_team"].str.normalize("NFKC")
+    data["away_team"] = data["away_team"].str.normalize("NFKC")
+    return data
 
-def check_day(day, _list):
-    year = 
-
-def check_holiday(day,_list):
-    if day in holiday:
-        _list.append(1)
-    else:
-        _list.append(0)
-
-def check_class(class_list,class_parts,_list):
-    a = class_list.index(class_parts)
-    for i in range(len(class_list)):
-        if i == a:
-            _list.append(1)
-        else:
-            _list.append(0)
-        
-
-def check_stadium(stadium_name,_list):
-    a = stadium[stadium_name]
-    _list.append(a)
-
-def comparison_class(class_list,class_parts,_list):
-    check = True
-    for k in class_list:
-        if float(class_parts) < k and check:
-            _list.append(1)
-            check = False
-        else:
-            _list.append(0)
-
-def crawling():
-    # クラス分け
-    list_data = np.array(train_data).T
-    list_data_test = np.array(test_data).T
-
-    # 日にち
-    _day = list_data[1].tolist()
-    month = []
-    day = []
-    for i, k in enumerate(_day):
-        month.append(int(k[6:8]))
-        day.append(int(k[9:11]))
-
-    # 時間
-    _time = list_data[2].tolist()
-    for i, k in enumerate(_time):
-        _time[i] = int(k[0:2])
-    max_time = np.amax(_time)
-    min_time = np.amin(_time)
-    step = 1 + math.log2(len(_time))
-    time_list = np.arange(min_time, max_time, (max_time-min_time)/step)
-
-    # 節
-    _section = list_data[3].tolist()
-    for i, k in enumerate(_section):
-        _section[i] = int(k[1])
-    max_section = np.amax(_section)
-    min_section = np.amin(_section)
-    step = 1 + math.log2(len(_section))
-    section_list = np.arange(min_section, max_section,(max_section-min_section)/step)
-
-    # 日
-    round_class = np.unique(np.hstack((list_data[4], list_data_test[4])))
-    round_list = round_class.tolist()
-
-    # チーム
-    _team = list_data[5].tolist()
-    _team3 = list_data[6].tolist()
-    _team2 = list_data_test[5].tolist()
-    _team4 = list_data_test[6].tolist()
-    for i, k in enumerate(_team):
-        list_data[5][i] = unicodedata.normalize("NFKC", k)
-    for i, k in enumerate(_team3):
-        list_data[6][i] = unicodedata.normalize("NFKC", k)
-    for i, k in enumerate(_team2):
-        list_data_test[5][i] = unicodedata.normalize("NFKC", k)
-    for i, k in enumerate(_team4):
-        list_data_test[6][i] = unicodedata.normalize("NFKC", k)
-    all_team = np.hstack((list_data[5], list_data_test[5]))
-    team_class = np.unique(all_team)
-    team_list = team_class.tolist()
-
-    # 天気
-    weather_class = np.unique(np.hstack((list_data[8], list_data_test[8])))
-    weather_list = weather_class.tolist()
-
-    # 気温
-    temperature_list = list(map(float, list_data[9].tolist()))
-    max_temperature = np.amax(temperature_list)
-    min_temperature = np.amin(temperature_list)
-    step = 1 + math.log2(len(temperature_list))
-    temperature_list = np.arange(min_temperature, max_temperature, (max_temperature-min_temperature)/step)
-
-    # 湿度
-    humidity_list = list(map(float, list_data[10].tolist()))
-    max_humidity = np.amax(humidity_list)
-    min_humidity = np.amin(humidity_list)
-    step = 1 + math.log2(len(humidity_list))
-    humidity_list = np.arange(min_humidity, max_humidity, (max_humidity-min_humidity)/step)
-
-    return time_list, section_list, round_list, team_list, weather_list, temperature_list, humidity_list
-
-time_list, section_list, round_list, team_list, weather_list, temperature_list, humidity_list = crawling()
 def get_train_data():
-    teacher_list = []
-    output_train_data = []
-    for i in train_data:
-        line = []
-        line.append(int(i[0]))
-        check_holiday(i[1], line)
-        comparison_class(time_list, int(i[2][0:2]), line)
-        comparison_class(section_list, int(i[3][1]), line)
-        check_class(round_list, i[4], line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[5]), line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[6]), line)
-        check_stadium(i[7], line)
-        check_class(weather_list, i[8], line)
-        comparison_class(temperature_list, i[9], line)
-        comparison_class(humidity_list, i[10], line)
-        teacher_list.append(i[-1])
-        output_train_data.append(line)
+    data = get_data()
 
-    # 学習データの出力
-    with open("data/train_data.csv", "w") as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(output_train_data)
+    # トレーニングデータのみを抽出
+    data = data[data["id"] < 18200]
 
-    # 教師データの出力
-    with open("data/teacher.csv", "w") as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(teacher_list)
+    # idを削除
+    data = data.drop("id", axis = 1)
+    
+    # csvとして出力
+    pd.get_dummies(data, drop_first=True).to_csv("data/train_data.csv", index=False)
+    teacher.to_csv("data/teacher.csv", index=False)
 
 def get_test_data():
-    output_test_data = []
-    for i in test_data[:459]:
-        line = []
-        line.append(int(i[0]))
-        check_holiday(i[1], line)
-        comparison_class(time_list, int(i[2][0:2]), line)
-        comparison_class(section_list, int(i[3][1]), line)
-        check_class(round_list, i[4], line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[5]), line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[6]), line)
-        check_stadium(i[7], line)
-        check_class(weather_list, i[8], line)
-        comparison_class(temperature_list, i[9], line)
-        comparison_class(humidity_list, i[10], line)
-        output_test_data.append(line)
-    
-    output_future_test_data = []
-    for i in test_data[459:]:
-        line = []
-        line.append(int(i[0]))
-        check_holiday(i[1], line)
-        comparison_class(time_list, int(i[2][0:2]), line)
-        comparison_class(section_list, int(i[3][1]), line)
-        check_class(round_list, i[4], line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[5]), line)
-        check_class(team_list, unicodedata.normalize("NFKC", i[6]), line)
-        check_stadium(i[7], line)
-        output_future_test_data.append(line)
-    
-    # テストデータの出力
-    with open("data/test_data.csv", "w") as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(output_test_data)
+    data = get_data()
 
-    # 未来のテストデータの出力
-    with open("data/future_test_data.csv", "w") as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows(output_future_test_data)
+    # テストデータのみを抽出
+    data = data[data["id"] > 19075]
 
-get_train_data()
-get_test_data()
+    # idを削除
+    data = data.drop("id", axis = 1)
+
+    # csvとして出力
+    pd.get_dummies(data, drop_first=True).to_csv("data/test_data.csv", index=False)
+
+    # 未来のデータと過去のデータに分割
+
+if __name__ == "__main__":
+    get_train_data()
+    get_test_data()
--- train.py
+++ train.py
@@ -1,67 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import GridSearchCV, cross_val_score
 import xgboost
-import numpy as np
-import csv
-import math
-import pickle
-from sklearn.model_selection import cross_val_score, GridSearchCV
-
-# 訓練データの読み込み
-train_data = []
-with open("data/train_data.csv", "r") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        train_data.append(list(map(int,row)))
-X = np.array(train_data)
-
-# 教師データの読み込み
-teacher_data = []
-with open("data/teacher.csv", "r") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        teacher_data = list(map(int,row))
-y = np.array(teacher_data)
 
 
-# xgboostの初期化
-mod = xgboost.XGBRegressor()
-X = np.delete(X,0,1)
-# ハイパーパラメータ探索
-reg_cv = GridSearchCV(
-    mod, {'max_depth': range(10), 'n_estimators': [50, 100, 200]}, verbose=1)
-reg_cv.fit(X, y)
-mod = xgboost.XGBRegressor(**reg_cv.best_params_)
+def get_data():
+  return pd.read_csv("data/train_data.csv"), pd.read_csv("data/teacher.csv", header=None)
 
-# 学習
+def full_train():
+  train, teacher = get_data()
 
-mod.fit(X,y)
+  # 学習モデル
+  mod = xgboost.XGBRegressor()
+  
+  mod.fit(train, teacher)
 
-# モデルの出力
-pickle.dump(mod, open("data/model.pkl","wb"))
+  # 交差検証
+  scores = cross_val_score(mod, train, teacher, cv=11)
+  print('Cross-Validation scores: {}'.format(scores))
 
+  """
+  mod_cv = GridSearchCV(mod, {'max_depth': [2,4,6], 'n_estimators': [50,100,200]}, verbose=1)
+  scores_cv = cross_val_score(mod_cv, train, teacher)
+  print('Cross-Validation scores: {}'.format(scores_cv))
+  """
 
-# 天気、湿度情報を抜いての学習
-_X = X.T[0:93].T
-_X = np.delete(_X, 0, 1)
-
-# xgboostの初期化
-mod2 = xgboost.XGBRegressor()
-
-# ハイパーパラメータ探索
-reg_cv = GridSearchCV(
-    mod2, {'max_depth': range(10), 'n_estimators': [50, 100, 200]}, verbose=1)
-reg_cv.fit(X, y)
-mod2 = xgboost.XGBRegressor(**reg_cv.best_params_)
-
-
-# 学習
-mod2.fit(_X, y)
-
-# モデルの出力
-pickle.dump(mod2, open("data/model2.pkl", "wb"))
-
-# 交差検証
-scores = cross_val_score(mod, X, y)
-scores2 = cross_val_score(mod2, _X, y)
-# スコアの平均値
-print('Average score: {}'.format(np.mean(scores)))
-print('Average score2: {}'.format(np.mean(scores2)))
+full_train()
